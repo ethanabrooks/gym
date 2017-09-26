f1e8983ecda7e44532f084fdfcc5fd9e5fedbbd2
Ethan Brooks <ethanabrooks@gmail.com>
2017-09-26 17:03:42 -0400

    removed import of other mujoco envs

--- gym/envs/mujoco/__init__.py
+++ gym/envs/mujoco/__init__.py
@@ -1,6 +1,7 @@
 from gym.envs.mujoco.mujoco_env import MujocoEnv
 # ^^^^^ so that user gets the correct error
 # message if mujoco is not installed correctly
+"""
 from gym.envs.mujoco.ant import AntEnv
 from gym.envs.mujoco.half_cheetah import HalfCheetahEnv
 from gym.envs.mujoco.hopper import HopperEnv
@@ -14,3 +15,4 @@ from gym.envs.mujoco.humanoidstandup import HumanoidStandupEnv
 from gym.envs.mujoco.pusher import PusherEnv
 from gym.envs.mujoco.thrower import ThrowerEnv
 from gym.envs.mujoco.striker import StrikerEnv
+"""
