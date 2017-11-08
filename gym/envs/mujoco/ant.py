d32a7517a92887981601dc54218e8ab708185ffc
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-08 13:34:43 -0500

    part way through porting to mjpro150

--- gym/envs/mujoco/ant.py
+++ gym/envs/mujoco/ant.py
@@ -8,18 +8,18 @@ class AntEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         utils.EzPickle.__init__(self)
 
     def _step(self, a):
-        xposbefore = self.get_body_com("torso")[0]
+        xposbefore = self.data.get_body_xpos("torso")[0]
         self.do_simulation(a, self.frame_skip)
-        xposafter = self.get_body_com("torso")[0]
-        forward_reward = (xposafter - xposbefore)/self.dt
+        xposafter = self.data.get_body_xpos("torso")[0]
+        forward_reward = (xposafter - xposbefore) / self.dt
         ctrl_cost = .5 * np.square(a).sum()
         contact_cost = 0.5 * 1e-3 * np.sum(
-            np.square(np.clip(self.model.data.cfrc_ext, -1, 1)))
+            np.square(np.clip(self.data.cfrc_ext, -1, 1)))
         survive_reward = 1.0
         reward = forward_reward - ctrl_cost - contact_cost + survive_reward
         state = self.state_vector()
         notdone = np.isfinite(state).all() \
-            and state[2] >= 0.2 and state[2] <= 1.0
+                  and 0.2 <= state[2] <= 1.0
         done = not notdone
         ob = self._get_obs()
         return ob, reward, done, dict(
@@ -30,9 +30,9 @@ class AntEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
     def _get_obs(self):
         return np.concatenate([
-            self.model.data.qpos.flat[2:],
-            self.model.data.qvel.flat,
-            np.clip(self.model.data.cfrc_ext, -1, 1).flat,
+            self.data.qpos.flat[2:],
+            self.data.qvel.flat,
+            np.clip(self.data.cfrc_ext, -1, 1).flat,
         ])
 
     def reset_model(self):
