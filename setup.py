559d8127b47d53f479ad738789383660533f2a3c
Ethan Brooks <ethanabrooks@gmail.com>
2022-08-12 22:19:43 -0400

    Remove importlib_metadata requirement

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     install_requires=[
         "numpy >= 1.18.0",
         "cloudpickle >= 1.2.0",
-        "importlib_metadata >= 4.8.0; python_version < '3.10'",
+        # "importlib_metadata >= 4.8.0; python_version < '3.10'",
         "gym_notices >= 0.0.4",
         "dataclasses == 0.8; python_version == '3.6'",
     ],
